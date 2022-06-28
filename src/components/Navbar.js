5f022cfaa306b19b3cf2c8ff6f3204b1dd8b07f9
Olaotan Abarowei <olaotanabarowei@gmail.com>
2022-06-28 20:32:30 +0100

    fixed bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,7 @@ const Navbar = () => {
         className="navbar navbar-expand-md navbar-dark bg-none"
       >
         <div className="container-fluid">
-          <a className="olaotan" href="!#">
-            Olaotan
-          </a>
+          <a className="olaotan">Olaotan</a>
           <button
             className="navbar-toggler hamburger"
             type="button"
