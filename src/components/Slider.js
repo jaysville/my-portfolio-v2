c1c7d0f3e820701feed352e9a3152300dcbf0d0e
Olaotan Abarowei <olaotanabarowei@gmail.com>
2022-06-28 21:10:48 +0100

    fixed bug

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,41 +1,21 @@
 import Carousel from "react-bootstrap/Carousel";
-import CarouselItem from "react-bootstrap/esm/CarouselItem";
 
 const Slider = (props) => {
-  const img = props.data;
+  const imgs = props.data;
 
   return (
     <Carousel>
-      <Carousel.Item>
-        <img
-          src={img[0]}
-          className="d-block w-100 img-thumbnail projectImgs"
-          alt="..."
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          src={img[1]}
-          className="d-block w-100 img-thumbnail projectImgs"
-          alt="..."
-        />
-      </Carousel.Item>
-      <CarouselItem>
-        <img
-          src={img[2]}
-          className="d-block w-100 img-thumbnail projectImgs"
-          alt="..."
-        />
-      </CarouselItem>
-      {img[3] && (
-        <CarouselItem>
-          <img
-            src={img[3]}
-            className="d-block w-100 img-thumbnail projectImgs"
-            alt="..."
-          />
-        </CarouselItem>
-      )}
+      {imgs.map((img, i) => {
+        return (
+          <Carousel.Item key={i}>
+            <img
+              src={img}
+              className="d-block w-100 img-thumbnail projectImgs"
+              alt="..."
+            />
+          </Carousel.Item>
+        );
+      })}
     </Carousel>
   );
 };
